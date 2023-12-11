3ddd8db48cae9adb149e93d9335428742f7e1a6d
liuqi <boxingliuqi@163.com>
2023-12-11 12:04:30 +0800

    fix: fix

--- next.config.js
+++ next.config.js
@@ -57,24 +57,6 @@ const securityHeaders = [
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
-/**
- * @type {import('next').NextConfig}
- */
-
-const nextConfig = {
-  output: 'export',
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
-
-module.exports = nextConfig
 
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
@@ -102,6 +84,7 @@ module.exports = () => {
     //     },
     //   ]
     // },
+    output: 'export',
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
