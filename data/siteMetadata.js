2d4fadd6a99ac52c27bd6634ec7b6009ab346747
liuqi <boxingliuqi@163.com>
2023-12-12 18:30:17 +0800

    feat: about

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,16 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth111',
+  title: 'lkk Blog',
+  author: '留奇',
   headerTitle: '留奇lkk',
-  description: '天道昭昭，变者恒通',
+  description: '大道易得，一窍难求',
   language: 'en-us',
-  theme: 'dark', // system, dark or light
+  theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  twitter: 'https://twitter.com/lkkliuqi',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
