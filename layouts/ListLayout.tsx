265feb0a736ed25083381b6ff68f15fe054b5739
liuqi <boxingliuqi@163.com>
2024-12-04 16:41:41 +0800

    feat:new articles

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -94,7 +94,7 @@ export default function ListLayout({
                 className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
               />
             </label>
-            <svg
+            {/* <svg
               className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -107,7 +107,7 @@ export default function ListLayout({
                 strokeWidth={2}
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               />
-            </svg>
+            </svg> */}
           </div>
         </div>
         <ul>
